21ad4131c5f4c579f8f34c698fa61c96d6f48b77
meowcodes <meowcodes@gmail.com>
2019-02-14 12:38:33 -0800

    update routes

--- app.py
+++ app.py
@@ -10,14 +10,20 @@ debug = DebugToolbarExtension(app)
 
 @app.route("/")
 def view_survey():
-    session['answers'] = []
+
+    
     return render_template('survey-intro.html', survey_title=satisfaction_survey.title,survey_instructions=satisfaction_survey.instructions)
 
 @app.route("/question/<int:question_num>", methods=["POST"])
 def view_question(question_num):
     next_question = question_num + 1
     the_question = satisfaction_survey.questions[question_num].question
+
     if question_num > 0:
-        session['answers'].append(request.args.get("answer_input"))
+        answers = session['answers']
+        answers.append(request.args.get("answer_input"))
+        session['answers'] = answers
+    elif:
+        session['answers'] = []
 
     return render_template('question.html', next_question = next_question, the_question=the_question )
